0fef22cd09bef24c55c760446d4751ba4151b302
Brandon Perez <brandonp940@gmail.com>
2020-06-17 13:16:55 -0400

    time, date, adding forms for rows now

--- script.js
+++ script.js
@@ -1,31 +1,48 @@
-// time for topm of page
-var dateTime = new Date();
-document.getElementById("currentDay").innerHTML = dateTime.toLocaleDateString();
-
+// date for top of page
+var date = new Date();
+document.getElementById("currentDay").innerHTML = date.toLocaleDateString();
+//time for top of page
+var dt = new Date();
+var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+document.write(time);
 // array for am numbers
-var amNum = ["12am","1am","2am","3am","4am","5am","6am","7am","8am","9am","10am","llam"];
+var amNum = ["12am","1am","2am","3am","4am","5am","6am","7am","8am","9am","10am","11am"];
 console.log(amNum);
 // array for pm numbers
 var pmNum = ["12pm","1pm","2pm","3pm","4pm","5pm","6pm","7pm","8pm","9pm","10pm","11pm"];
 console.log(pmNum);
 
-//varibale for id on html using jquery
-var bodyDiv = $(".a");
-console.log(bodyDiv);
+// loop to put individual hours for am in block
+function getTime() {
+  for (var i= 0;i < amNum.length ;i ++) {
+    document.write("<table border=1>");
+    document.write("<tr><td width=1250px height=100px>" + amNum[i] + "</td></tr>");
+    document.write("</table>");
+}return "";
 
-// for loop for amNumber array
-for (let i = 0; i < amNum.length; i++) {
-   //variable that creates new time blocks
-   var newTimeBlocks = $("<div class= card> <div class= card-body> <p id='timeblocks1' class= 'tb1'></p></div></div>");
-    console.log(newTimeBlocks);
-    //add times from array
-    var timeblock = $(".timeblocks");
-    console.log(timeblock);
-   $(timeblock).text(amNum);
-   // add to bodyDiv
-  bodyDiv.append(newTimeBlocks);
-    
 }
 
- 
+document.getElementById("times").innerHTML=getTime();
+console.log(getTime);
 
+// loop to put individual hours for am in block
+function getTime2() {
+  for (var i= 0;i < pmNum.length ;i ++) {
+    document.write("<table border=1>");
+    document.write("<tr><td width=1250px height=100px>" + pmNum[i] + "</td></tr>");
+    document.write("</table>");
+}return "";
+}
+
+document.getElementById("times2").innerHTML=getTime2();
+console.log(getTime2);
+
+//form for user to put info
+var formDiv = $("#form");
+// loop for forms
+for (i = 0; i < amNum.length; i++) {
+  // variable for form
+  var userInput = $("<form><input type='text' id='formBox' name'box' value'Enter Event'></form>");
+  console.log(userInput);
+  $(formDiv).append(userInput);
+}
